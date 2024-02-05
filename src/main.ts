8261e7c66eba18258635a5bfddbba4f4e900c4c9
KHARERISHABH <rishabkhare41@gmail.com>
2024-02-05 23:44:14 +0530

    services in standalone component

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode } from "@angular/core";
 
-import { environment } from './environments/environment';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { environment } from "./environments/environment";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+import { AnalyticsService } from "./app/shared/analytics.service";
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent)
+bootstrapApplication(AppComponent, { 
+  providers: [AnalyticsService] 
+});
